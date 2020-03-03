55bce1db528a559380ce623234a17b7718183061
int55 <iga-pro555@bk.ru>
2020-03-03 16:53:32 +0300

    Status bar is hidden in the file MainActivity.java

--- app/src/main/java/com/example/android/toolsshopapp/MainActivity.java
+++ app/src/main/java/com/example/android/toolsshopapp/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
         //скрываем actionBar
         ActionBar actionBar = getSupportActionBar();
         if(actionBar != null) actionBar.hide();
+
+        //скрываем строку состояния в самом верху
+        Window window = getWindow();
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         // активность приветствия с лого
         new Handler().postDelayed(new Runnable() {
